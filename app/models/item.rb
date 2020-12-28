882e0b5c39975c5d6000bbbf17750815ed5e359b
hiroforgithub <herohiro505@gmail.com>
2020-12-28 15:02:14 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,22 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :category, presence: true
+  validates :prefecture, presence: true
+  validates :shipping_date, presence: true
+  validates :shipping_fee, presence: true
+  validates :status, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :prefecture
   belongs_to :shipping_date
   belongs_to :shipping_fee
   belongs_to :status
+  belongs_to :user
+  has_one_attached :image
+  has_one :buy
+
 end
