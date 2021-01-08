2ec188054214d2dddda13338736832bb025147d4
hiroforgithub <herohiro505@gmail.com>
2021-01-08 09:19:14 +0900

    鍵情報を環境変数として設定

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -9,6 +9,7 @@ class BuyController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid? 
+      pay_item
       @buy_address.save
       redirect_to root_path
     else
@@ -18,8 +19,17 @@ class BuyController < ApplicationController
 
   private
   def buy_params
-    params.permit(:code, :prefecture_id, :city, :street, :building, :tel, :item_id).merge(user_id: current_user.id)
+    params.require(:buy_address).permit(:code, :prefecture_id, :city, :street, :building, :tel).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述
+    Payjp::Charge.create(
+      amount: @item[:price],  # 商品の値段
+      card: buy_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+  end  
 
 end
 
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,21 +1,22 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :code, :prefecture_id, :city, :street, :building, :tel, :buy_id
+  attr_accessor :item_id, :user_id, :code, :prefecture_id, :city, :street, :building, :tel, :buy_id, :token
 
   with_options presence: true do
     validates :code
     validates :prefecture_id
     validates :city
     validates :street
-    validates :building
+    # validates :building
     validates :tel
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
     # 購入の情報を保存
-    buy =  Buy.create(item_id: item_id, user_id: user_id)
+    buy = Buy.create(item_id: item_id, user_id: user_id)
     # 住所の情報を保存
     Address.create(code: code, prefecture_id: prefecture_id, city: city, street: street, building: building, tel: tel, buy_id: buy.id)
   end
